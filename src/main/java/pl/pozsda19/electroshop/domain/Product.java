692d672a387fa015c8459cc248d776ab1ea8133f
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-04-25 20:35:46 +0200

    klasa Product z polami, enum Category, enum SubCategory

---
+++ src/main/java/pl/pozsda19/electroshop/domain/Product.java
@@ -0,0 +1,22 @@
+package pl.pozsda19.electroshop.domain;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private Long id;
+
+    private Category category;
+
+    private SubCategory subCategory;
+
+    private String pictureURL;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+
+}
