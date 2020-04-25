a8004a51c6a8b4a237901d828aae290999e28528
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-04-25 18:29:24 +0200

    first Commit

---
+++ src/main/java/pl/pozsda19/electroshop/ElectroshopApplication.java
@@ -0,0 +1,13 @@
+package pl.pozsda19.electroshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectroshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pozsda19/electroshop/ElectroshopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pozsda19.electroshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
