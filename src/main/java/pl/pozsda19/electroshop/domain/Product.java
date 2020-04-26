3d2a3451a2b2dc059afe7b513185cbddb5dbcc10
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-04-26 09:07:14 +0200

    dodane dwa pola productName i productCode - przeszukiwanie jest po nazwie produktu lub kodzie.

--- src/main/java/pl/pozsda19/electroshop/domain/Product.java
+++ src/main/java/pl/pozsda19/electroshop/domain/Product.java
@@ -1,10 +1,17 @@
 package pl.pozsda19.electroshop.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
-
+import java.util.Objects;
+@Entity
 public class Product {
-
+    @Id
     private Long id;
+
+    private String productName;
+
+    private String productCode;
 
     private Category category;
 
@@ -18,5 +25,99 @@ public class Product {
 
     private Integer quantity;
 
+    public Product() {
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public SubCategory getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(SubCategory subCategory) {
+        this.subCategory = subCategory;
+    }
+
+    public String getPictureURL() {
+        return pictureURL;
+    }
+
+    public void setPictureURL(String pictureURL) {
+        this.pictureURL = pictureURL;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) &&
+                Objects.equals(productName, product.productName) &&
+                Objects.equals(productCode, product.productCode) &&
+                category == product.category &&
+                subCategory == product.subCategory &&
+                Objects.equals(pictureURL, product.pictureURL) &&
+                Objects.equals(description, product.description) &&
+                Objects.equals(price, product.price) &&
+                Objects.equals(quantity, product.quantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, productName, productCode, category, subCategory, pictureURL, description, price, quantity);
+    }
 }
