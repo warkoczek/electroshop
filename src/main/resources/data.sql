db2d2a9ce6f4c7d7421f2f685896de83f8b64035
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-04-26 09:41:25 +0200

    data.sql tworzymy. wprowadziłem poziomice. Czasowo, pole description i quantity zostaje domyslne. Zdjecie czasowo podajemy jako url do istnejącej strony. Czy właściciel posiada baze zdjęć, bo jesli istniejąca strona wygaśnie to nasze URL też. Czy mamy własne zdjęcia zaporoponować?

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert  into PRODUCTS(id, product_Name, product_Code, category, subcategory, picture_URL, description, price, quantity)
+values (nextval('product_Seq'), 'Poziomica cyfrowa BOSCH DNM 60L', '0601014000', 'TECHNIKAPOMIAROWA'
+, 'POZIOMICA', 'http://www.elektroserw.pl/zdjecia/47_1.jpg', 'description', 850, 1)
