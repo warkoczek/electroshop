1fbb26981bbcf2cc1b5c71afc03eef55855d5bdc
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-05-10 15:06:59 +0200

    createProduct

--- src/main/java/pl/pozsda19/electroshop/controller/mvc/ProductManagementMVCController.java
+++ src/main/java/pl/pozsda19/electroshop/controller/mvc/ProductManagementMVCController.java
@@ -5,38 +5,53 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
-import pl.pozsda19.electroshop.domain.Product;
+import pl.pozsda19.electroshop.domain.dto.ProductEntityReading;
 import pl.pozsda19.electroshop.domain.dto.ProductEntityWriting;
 import pl.pozsda19.electroshop.service.ProductManagementService;
+import pl.pozsda19.electroshop.service.ProductService;
 
-import javax.validation.Valid;
+import java.util.Set;
+
 
 @Controller
 @RequestMapping("/productAdministration")
 public class ProductManagementMVCController {
 
     private final ProductManagementService productManagementService;
+    private final ProductService productService;
 
-    public ProductManagementMVCController(ProductManagementService productManagementService) {
+    public ProductManagementMVCController(ProductManagementService productManagementService, ProductService productService) {
         this.productManagementService = productManagementService;
+        this.productService=productService;
     }
 
-    @GetMapping(value = "/addProduct")
-    public String getAddProductForm(){
+    @GetMapping(value = "/showProducts")
+    public ModelAndView showProducts() {
+        ModelAndView modelAndView = new ModelAndView("showProductsList");
+        Set<ProductEntityReading> products = productService.showAllProducts();
+        modelAndView.addObject("products", products);
+        return modelAndView;
+    }
+
+    @GetMapping("/addProduct")
+    public String showAddProductForm(){
         return "addProduct";
     }
 
-    @PostMapping(value = "/add")
-    public ModelAndView createProduct(@ModelAttribute ProductEntityWriting productEntityWriting){
-        ModelAndView modelAndView = new ModelAndView("addProduct");
-        Product product = productManagementService.createProduct(productEntityWriting);
-
-        return null;
+    @PostMapping("/addProduct")
+    public String createProduct(@ModelAttribute("product") ProductEntityWriting productEntityWriting, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "addProduct";
+        }
+        productManagementService.createProduct(productEntityWriting);
+        model.addAttribute("product", new ProductEntityWriting());
+        model.addAttribute("message", "Dodano product");
+        return "addProduct";
     }
 
 
--- src/main/java/pl/pozsda19/electroshop/service/ProductManagementService.java
+++ src/main/java/pl/pozsda19/electroshop/service/ProductManagementService.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Service;
 import pl.pozsda19.electroshop.domain.Product;
 import pl.pozsda19.electroshop.domain.dto.ProductEntityWriting;
 import pl.pozsda19.electroshop.domain.dto.ProductMapper;
+import pl.pozsda19.electroshop.exception.DuplicateProductCodeException;
 import pl.pozsda19.electroshop.repository.ProductRepository;
+
+import java.util.Optional;
 
 @Service
 public class ProductManagementService {
@@ -19,12 +22,15 @@ public class ProductManagementService {
 
     public Product createProduct(ProductEntityWriting toProductEntity){
 
+        Optional<Product> productByCode = productRepository.findByCode(toProductEntity.getCode()).orElse();
+        if(productByCode.isPresent()){
+            throw new DuplicateProductCodeException("code reserved");
+        }
         Product product = productMapper.writeProductEntity(toProductEntity);
 
         product =  productRepository.save(product);
 
         return product;
-
     }
 
 
